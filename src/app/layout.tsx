2f63ce2ec9f68a01734383233097da754153ac51
Yaswanth Mallisetti <mallisettiyaswanth@gmail.com>
2024-12-28 11:55:00 +0530

    bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.rings.min.js"></script>
     </html>
   );
 }

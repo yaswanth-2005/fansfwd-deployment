73509e441fb65704b4d3a44b2c289b4e0835b6cc
Yaswanth Varada <yaswanthvarada20@gmail.com>
2025-01-02 11:01:40 +0530

    basci

--- src/components/global/inView.tsx
+++ src/components/global/inView.tsx
@@ -23,16 +23,16 @@ export function InViewImagesGrid() {
         >
           <div className="columns-2 gap-4 px-8 sm:columns-3">
             {[
-              "https://images.beta.cosmos.so/e5ebb6f8-8202-40ec-bc70-976f81153501?format=jpeg",
               "https://images.beta.cosmos.so/1b6f1bee-1b4c-4035-9e93-c93ef4d445e1?format=jpeg",
-              "https://images.beta.cosmos.so/9968a6cf-d7f6-4ec9-a56d-ac4eef3f8689?format=jpeg",
-              "https://images.beta.cosmos.so/4b88a39c-c657-4911-b843-b473237e83b5?format=jpeg",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEODqeCcEuXN4GYFNu1Doh7dS5gomDgKC2wQ&s",
+              "https://yt3.googleusercontent.com/m6Lq0zDJ8qQfipdpk-bWFVUswXZhnxNbIJH7qgv6wv_RnjcYvZrrIYj--Nz3rDd_m5BSLfUYaQ=s176-c-k-c0x00ffffff-no-rj-mo",
               "https://images.beta.cosmos.so/86af92c0-064d-4801-b7ed-232535b03328?format=jpeg",
               "https://images.beta.cosmos.so/399e2a4a-e118-4aaf-9c7e-155ed18f6556?format=jpeg",
-              "https://images.beta.cosmos.so/6ff16bc9-dc94-4549-a057-673a603ce203?format=jpeg",
-              "https://images.beta.cosmos.so/d67c3185-4480-4408-8f9d-1cbf541e5d91?format=jpeg",
-              "https://images.beta.cosmos.so/a7b19274-3370-4080-b734-e8ac268d8c8e.?format=jpeg",
-              "https://images.beta.cosmos.so/551daf0d-77e8-472c-9324-468fed15a0ba?format=jpeg",
+              "https://th.bing.com/th/id/OIP.StotjN4kdzLr-wgoZQ-xkAHaJP?w=156&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStLBs8iyDb-u0Mr9nGSjS9aZel-XG52NJMeA&s",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzWt2e9kG3EStglMVgYtf1e0_nu2G46zT0AQ&s",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7nfkloJmkIv2qSmORIOadcRqX_e2fAxKVdA&sjpeg",
+              "https://prasadtechintelugu.com/wp-content/uploads/2023/04/still-for-subscribe-834x1024.png",
             ].map((imgSrc, index) => {
               return (
                 <motion.div
